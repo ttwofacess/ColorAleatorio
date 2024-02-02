9b39e5b06e82736d6824408f2b8d842f40a3d984
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-02-02 09:19:49 -0300

    Crear funcion para hacer la paleta y generar sus elementos en body.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const palletteContainer = document.getElementById('palletteContainer');
 const colorValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 const PALLETE_SIZE = 5;
+
+const createPallete = () => {
+    for (let i = 0; i < PALLETE_SIZE; i++) {
+        const palleteElement = document.createElement('div');
+        palleteElement.classList.add('palleteItem');
+        palletteContainer.appendChild(palleteElement);
+    }
+    updatePallete();
+}
+
+createPallete();
