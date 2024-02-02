c42465d1276d239fef34eb977d0581ca07d080bc
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-02-02 09:15:11 -0300

    Definir valores en JS.

---
+++ script.js
@@ -0,0 +1,3 @@
+const palletteContainer = document.getElementById('palletteContainer');
+const colorValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+const PALLETE_SIZE = 5;
